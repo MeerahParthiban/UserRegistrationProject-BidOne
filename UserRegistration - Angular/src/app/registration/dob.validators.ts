6f77902bc8f660760c0df181cfd9a657f704b58c
MeerahParthiban <78683936+MeerahParthiban@users.noreply.github.com>
2021-02-19 16:50:57 +1300

    Adding comments to method

--- UserRegistration - Angular/src/app/registration/dob.validators.ts
+++ UserRegistration - Angular/src/app/registration/dob.validators.ts
@@ -1,6 +1,7 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 import * as moment from 'moment';
 export class DateOfBirthValidators{
+   //Method to validate Date of birth - dob is invalid if the entered date is greater than today
    static cannotBeAfterCurrentDate(control: AbstractControl):ValidationErrors | null{
        if(control.value!=null){
            const dob = moment(control.value);
